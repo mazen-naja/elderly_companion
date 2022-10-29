7862a3f94f2fc297c7388361a4e1248a55f754a0
Mazennaja123 <mazennajaa@gmail.com>
2022-10-29 07:11:54 +0300

    Updating Database-Users table

--- backend/server/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('gender');
+            $table->integer('age');
+            $table->integer('user_type');
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
