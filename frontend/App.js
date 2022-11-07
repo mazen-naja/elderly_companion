2c768e90a238e90863ddeb65daee9d5381fea930
Mazennaja123 <mazennajaa@gmail.com>
2022-11-07 06:43:09 +0200

    Creating Role screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,27 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from "react";
 import { Text, View ,TouchableOpacity, TextInput ,Image } from 'react-native';
-import RoleScreen from './src/screens/RoleScreen/RoleScreen';
-import styles from './styles';
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import RoleScreen from './src/screens/RoleScreen/RoleScreen.js';
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from 'react-navigation-stack';
+import ElderLogin from './src/screens/ElderLogin/ElderLogin.js';
+// import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createNativeStackNavigator();
-export default function App() {
-   return (
-     <NavigationContainer>
-       <Stack.Navigator>
-         <Stack.Screen 
-           name="RoleScreen"
-           component={RoleScreen}
+
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//    return (
+//       <NavigationContainer>
+//        <Stack.Navigator>
+//          <Stack.Screen 
+//            name="RoleScreen"
+//            component={RoleScreen}
           
-         />
-         <Stack.Screen 
-           name="ElderSignin"
-           component={ElderSignin}
-         />
+//          />
+//          <Stack.Screen 
+//            name="ElderLogin"
+//            component={ElderLogin}
+//          />
 
-       </Stack.Navigator>
-     </NavigationContainer>
-   );
- }
+//        </Stack.Navigator>
+//       </NavigationContainer>
+//    );
+//  }
--- frontend/src/screens/RoleScreen/index.tsx
+++ frontend/src/screens/RoleScreen/index.tsx
@@ -4,21 +4,19 @@ import { Text, View ,Button ,TouchableOpacity, TextInput ,Image } from 'react-na
 import styles from "../../../styles";
 
 
-
-export default function RoleScreen({ navigation }) {
-
+export default function App() {
   return (
     <View style={styles.container}>
-      <Image style={styles.tinyLogo} source={require('../../../assets/logo.jpeg')} />
+      <Image style={styles.tinyLogo} source={require('./assets/logo.jpeg')} />
       <Text style={styles.text_title}>Elderly Companion</Text>
       <Text style={styles.text_subtitle}>Choose role</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("ElderLogin")} style={styles.button}>
+      <TouchableOpacity style={styles.button}>
                   <Text style={styles.role_text}>Elder</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("ElderLogin")}>
+      <TouchableOpacity style={styles.button}>
                   <Text style={styles.role_text}>Caretaker</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
-};
+}
