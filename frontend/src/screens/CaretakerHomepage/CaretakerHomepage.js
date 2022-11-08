8204df4b1c1e985590693596eccd7306e8bc73ae
Mazennaja123 <mazennajaa@gmail.com>
2022-11-09 00:58:13 +0200

    Creating Caretaker homepage

---
+++ frontend/src/screens/CaretakerHomepage/CaretakerHomepage.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Text, View ,TouchableOpacity, TextInput ,Image } from 'react-native';
+import styles from "../../../styles";
+
+export default function App() {
+  return (
+
+
+    <View style={styles.container}>
+     
+      <Image style={styles.tinyLogo} source={require('../../../assets/oldmanicon.jpeg')} />
+      <Text style={styles.text_title}>Welcome Caretaker !</Text>
+      <View style={styles.container2}>
+          <TouchableOpacity flex style={styles.homecard}>
+                            <Image style={styles.cardicon} source={require('../../../assets/scheduleicon.png')} />
+                            <Text style={styles.card_text}>Schedule</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.homecard}>
+          <Image style={styles.cardicon} source={require('../../../assets/caretakers.png')} />
+                            <Text style={styles.card_text}>Elders</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.homecard}>
+          <Image style={styles.cardicon} source={require('../../../assets/chat.png')} />
+                            <Text style={styles.card_text}>Chat</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.homecard}>
+          <Image style={styles.cardicon} source={require('../../../assets/editprofile.jpg')} />
+                            <Text style={styles.card_text}>Edit profile</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.homecard}>
+          <Image style={styles.cardicon} source={require('../../../assets/notificationsicon.png')} />
+                            <Text style={styles.card_text}>Notifications</Text>
+          </TouchableOpacity>
+      
+      </View>
+    </View>
+  );
+
+}
