549b1018ae4b7d8a159ee2fcb7d2682f2e8111ec
Mazen Naja <mazennajaa@gmail.com>
2022-11-18 19:36:12 +0200

    Restructuring homepage

--- frontend/src/screens/CaretakerHomepage/CaretakerHomepage.jsx
+++ frontend/src/screens/CaretakerHomepage/CaretakerHomepage.jsx
@@ -1,46 +1,72 @@
 import React, { useState } from "react";
 import { Text, View ,TouchableOpacity, TextInput ,Image } from 'react-native';
 import styles from "../../../styles";
+import { StatusBar } from "expo-status-bar";
+import Homepage_card from "../../../Components/Cards/HomepageCard";
+
+
 
 <StatusBar style="auto" />
 
-const CaretakerHomepage=()=>{
+const CaretakerHomepage=({navigation})=>{
 
   return (
 
 
-    <View style={styles.container}>
-     
-      <Image style={styles.tinyLogo} source={require('../../../assets/oldmanicon.jpeg')} />
-      <Text style={styles.text_title}>Welcome Caretaker !</Text>
-      <View style={styles.container2}>
-          <TouchableOpacity flex style={styles.homecard}>
-                            <Image style={styles.cardicon} source={require('../../../assets/scheduleicon.png')} />
-                            <Text style={styles.card_text}>Schedule</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.homecard}>
-          <Image style={styles.cardicon} source={require('../../../assets/caretakers.png')} />
-                            <Text style={styles.card_text}>Elders</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.homecard}>
-          <Image style={styles.cardicon} source={require('../../../assets/chat.png')} />
-                            <Text style={styles.card_text}>Chat</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.homecard}>
-          <Image style={styles.cardicon} source={require('../../../assets/editprofile.jpg')} />
-                            <Text style={styles.card_text}>Edit profile</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.homecard}>
-          <Image style={styles.cardicon} source={require('../../../assets/notificationsicon.png')} />
-                            <Text style={styles.card_text}>Notifications</Text>
-          </TouchableOpacity>
+    <View style={styles.home_container}>
       
-      </View>
-    </View>
+    <Image style={styles.tinyLogo} source={require('../../../assets/caretaker.png')} />
+    <Text style={styles.text_title}>Welcome Caretaker !</Text>
+    
+        <View style={styles.flex_homerow2}>
+            {/* <Homepage_card card_text={'Schedule'} img_src={require('../../../assets/scheduleicon.png')}   /> */}
+        
+          <TouchableOpacity  style={styles.homecard} onPress={() =>  navigation.navigate("CaretakerScheduleScreen")}>
+                <View >
+                    <Image style={styles.cardicon} source={require('../../../assets/scheduleicon.png')} />
+                </View>
+                <Text style={styles.card_hometext}>Schedule</Text>
+         </TouchableOpacity>
+
+
+            {/* <Homepage_card card_text={'Caretakers'} img_src={require('../../../assets/caretakers.png')}  /> */}
+            <TouchableOpacity  style={styles.homecard} onPress={() =>  navigation.navigate("Caretakers_Elders")}>
+                <View >
+                    <Image style={styles.cardicon} source={require('../../../assets/caretakers.png')} />
+                </View>
+                <Text style={styles.card_hometext}>Caretakers</Text>
+            </TouchableOpacity>
+        </View >
+
+        <View style={styles.flex_homerow2}>
+          {/* <Homepage_card card_text={'Chat'} img_src={require('../../../assets/chat.png')} /> */}
+
+          <TouchableOpacity  style={styles.homecard} onPress={() =>  navigation.navigate("Caretakers_Elders")}>
+                <View >
+                    <Image style={styles.cardicon} source={require('../../../assets/chat.png')} />
+                </View>
+                <Text style={styles.card_hometext}>Chat</Text>
+            </TouchableOpacity>
+
+          {/* <Homepage_card card_text={'Edit profile'} img_src={require('../../../assets/editprofile.jpg')} /> */}
+          <TouchableOpacity  style={styles.homecard} onPress={() =>  navigation.navigate("Caretakers_Elders")}>
+                <View >
+                    <Image style={styles.cardicon} source={require('../../../assets/editprofile.jpg')} />
+                </View>
+                <Text style={styles.card_hometext}>Edit profile</Text>
+            </TouchableOpacity>
+        </View>
+
+
+        <View style={styles.flex_homerow2}>
+          <Homepage_card card_text={'Notifications'} img_src={require('../../../assets/notificationsicon.png')}/>
+
+
+
+
+          
+        </View>
+  </View>
   );
 
 };
