50c2ab6b9c353daf4f4a41c2467a39600e5d9350
Mazen Naja <mazennajaa@gmail.com>
2022-11-18 19:36:56 +0200

    Updating Navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,8 @@
-import ElderScheduleScreen from "./src/screens/ElderScheduleScreen/ElderScheduleScreen";
+// import ElderScheduleScreen from "./src/screens/ElderScheduleScreen/ElderScheduleScreen";
 import React, { useState } from "react";
-import { Text, View ,TouchableOpacity, TextInput ,Image } from 'react-native';
-import styles from "./styles";
-import ElderHomepage from "./src/screens/ElderHomepage/ElderHomepage";
+// import { Text, View ,TouchableOpacity, TextInput ,Image } from 'react-native';
+// import styles from "./styles";
+// import ElderHomepage from "./src/screens/ElderHomepage/ElderHomepage";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ElderLogin from "./src/screens/ElderLogin/ElderLogin";
@@ -10,12 +10,21 @@ import RoleScreen from "./src/screens/RoleScreen/RoleScreen";
 import CaretakerLogin from "./src/screens/CaretakerLogin/CaretakerLogin";
 import CaretakerSignUp from "./src/screens/CaretakerSignUp/CaretakerSignUp";
 import ElderSignUp from "./src/screens/ElderSignUp/ElderSignUp";
+import ChatList from "./src/screens/ChatList/ChatList";
+import CaretakerHomepage from "./src/screens/CaretakerHomepage/CaretakerHomepage";
+import CaretakerScheduleScreen from "./src/screens/CaretakerScheduleScreen/CaretakerScheduleScreen";
+import Caretakers_Elders from "./src/screens/Caretakers_Elders/Caretakers_Elders";
+
 
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
+  //  <ChatList/>
+  // <ElderHomepage/>
+  // <ElderScheduleScreen/>
+
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="RoleScreen" component={RoleScreen} options={{headerShown: false}}/>
@@ -23,6 +32,9 @@ export default function App() {
           <Stack.Screen name="ElderSignUp" component={ElderSignUp}options={{headerTransparent: true,headerTitle: ""}}/>
           <Stack.Screen name="CaretakerLogin" component={CaretakerLogin} options={{headerTransparent: true,headerTitle: ""}}/>
           <Stack.Screen name="CaretakerSignUp" component={CaretakerSignUp} options={{headerTransparent: true,headerTitle: ""}}/>
+          <Stack.Screen name="CaretakerHomepage" component={CaretakerHomepage} options={{headerTransparent: true,headerTitle: ""}}/>
+          <Stack.Screen name="CaretakerScheduleScreen" component={CaretakerScheduleScreen} options={{headerTransparent: true,headerTitle: ""}}/>
+          <Stack.Screen name="Caretakers_Elders" component={Caretakers_Elders} options={{headerTransparent: true,headerTitle: ""}}/>
         </Stack.Navigator>
       </NavigationContainer>
   );
