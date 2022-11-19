0e5627e3befad212d83ff5887cc22cbd46e99bb8
Mazen Naja <mazennajaa@gmail.com>
2022-11-19 07:44:13 +0200

    Modifying Elder homepage

--- frontend/src/screens/ElderHomepage/ElderHomepage.jsx
+++ frontend/src/screens/ElderHomepage/ElderHomepage.jsx
@@ -6,7 +6,7 @@ import Homepage_card from "../../../Components/Cards/HomepageCard";
 
 <StatusBar style="auto" />
 
-const ElderHomepage=()=>{
+const ElderHomepage=({navigation})=>{
 
   return (
     <View style={styles.home_container}>
@@ -15,13 +15,48 @@ const ElderHomepage=()=>{
       <Text style={styles.text_title}>Welcome Elder !</Text>
       
           <View style={styles.flex_homerow2}>
-              <Homepage_card card_text={'Schedule'} img_src={require('../../../assets/scheduleicon.png')}  />
-              <Homepage_card card_text={'Medicines'} img_src={require('../../../assets/medicines.jpeg')} />
+
+              {/* <Homepage_card card_text={'Schedule'} img_src={require('../../../assets/scheduleicon.png')}  /> */}
+              <TouchableOpacity  style={styles.homecard} onPress={() =>  navigation.navigate("ElderScheduleScreen")}>
+                <View >
+                    <Image style={styles.cardicon} source={require('../../../assets/scheduleicon.png')}/>
+                </View>
+                <Text style={styles.card_hometext}>Schedule</Text>
+            </TouchableOpacity>
+
+
+            
+              {/* <Homepage_card card_text={'Medicines'} img_src={require('../../../assets/medicines.jpeg')} /> */}
+              <TouchableOpacity  style={styles.homecard} onPress={() =>  navigation.navigate("Medicines")}>
+                <View >
+                    <Image style={styles.cardicon} source={require('../../../assets/medicines.jpeg')}/>
+                </View>
+                <Text style={styles.card_hometext}>Medicines</Text>
+            </TouchableOpacity>
+
+
           </View >
 
           <View style={styles.flex_homerow2}>
-            <Homepage_card card_text={'Caretakers'} img_src={require('../../../assets/caretakers.png')} />
-            <Homepage_card card_text={'Chat'} img_src={require('../../../assets/chat.png')}/>
+
+            {/* <Homepage_card card_text={'Caretakers'} img_src={require('../../../assets/caretakers.png')} /> */}
+
+            <TouchableOpacity  style={styles.homecard} onPress={() =>  navigation.navigate("EldersCaretakers")}>
+                <View >
+                    <Image style={styles.cardicon} source={require('../../../assets/caretakers.png')}/>
+                </View>
+                <Text style={styles.card_hometext}>Caretakers</Text>
+            </TouchableOpacity>
+
+
+            {/* <Homepage_card card_text={'Chat'} img_src={require('../../../assets/chat.png')}/> */}
+            <TouchableOpacity  style={styles.homecard} onPress={() =>  navigation.navigate("ElderChatlist")}>
+                <View >
+                    <Image style={styles.cardicon} source={require('../../../assets/chat.png')}/>
+                </View>
+                <Text style={styles.card_hometext}>Chat</Text>
+            </TouchableOpacity>
+
           </View>
 
           <View style={styles.flex_homerow2}>
