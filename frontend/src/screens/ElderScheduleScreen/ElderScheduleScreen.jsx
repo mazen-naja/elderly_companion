2d35f17480d5471b7dca2904e2f6799159b81d9e
Mazen Naja <mazennajaa@gmail.com>
2022-11-19 07:45:23 +0200

    Modifying Elder schedule screen

--- frontend/src/screens/ElderScheduleScreen/ElderScheduleScreen.jsx
+++ frontend/src/screens/ElderScheduleScreen/ElderScheduleScreen.jsx
@@ -9,9 +9,15 @@ const ElderScheduleScreen=()=>{
     return (
    
     <View style={styles.container}>
-      
       <Image style={styles.tinyLogo} source={require('../../../assets/scheduleicon.png')} />
-      <Text style={styles.text_title}>Schedule</Text>   
+      <Text style={styles.text_title}>Schedule</Text> 
+       
+      <View style={styles.flex_row_subtitle} >
+        <Text style={styles.chat_subtitle}>Today's Schedule</Text> 
+      <TouchableOpacity style={styles.subtitle_button}><Text >ADD</Text></TouchableOpacity>
+      </View>
+
+
       <Schedule_card text={'Walk'} time={'12:09pm'} img_src={require('../../../assets/walk.png')} />
     </View>
 
