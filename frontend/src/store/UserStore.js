6bdbbeab35851ad08ba9081307fb7143b1922c1c
Mazen Naja <mazennajaa@gmail.com>
2022-11-21 07:35:02 +0200

    Adding Store item values

--- frontend/src/store/UserStore.js
+++ frontend/src/store/UserStore.js
@@ -3,18 +3,22 @@ import { persist } from 'zustand/middleware'
 
 
 const useStore = create(
-    
+   
         (set) => ({
             user_id: null,
             profile_picture: "",
             token: 0,
-            usertype: "",
+            usertype: 0,
             elders_list:"",
+            login_state:"",
+            elder_schedule_list:"",
+            setLoginStatus: (login_status) => set((state) => ({ login_state: login_status })),
             setUser: (user) => set((state) => ({ user_id: user })),
             setProfile: (pic) => set((state) => ({ profile_picture: pic })),
             setToken: (tkn) => set((state) => ({ token: tkn })),
             setUsertype: (usrtype) => set((state) => ({ usertype: usrtype })),
             setEldersList: (arr) => set((state) => ({ elders_list: arr })),
+            setElderSchedule: (elder_schdeule) => set((state) => ({ elder_schedule_list: elder_schdeule })),
             removeUserId: () => set({ user_id: null }),
             removeProfile: () => set({ profile_picture: "" }),
             removeToken: () => set({ token: 0 }),
