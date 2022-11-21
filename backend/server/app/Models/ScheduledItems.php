baad529423e682fd442befa140a80cc121a6bb2f
Mazen Naja <mazennajaa@gmail.com>
2022-11-21 07:45:13 +0200

    Adding Scheduled items model

---
+++ backend/server/app/Models/ScheduledItems.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ScheduledItems extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+    
+];
+
+
+    protected $table='scheduled_items';
+}
