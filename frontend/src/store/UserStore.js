9845d0225ca868ec72fb6ea72d0b3049cce1d637
Mazen Naja <mazennajaa@gmail.com>
2022-11-22 20:17:44 +0200

    Adding set Schedule to store

--- frontend/src/store/UserStore.js
+++ frontend/src/store/UserStore.js
@@ -12,11 +12,16 @@ const useStore = create(
             elders_list:"",
             login_state:"",
             elder_schedule_list:"",
+            CaretakerSchedule:"",
+
             setLoginStatus: (login_status) => set((state) => ({ login_state: login_status })),
             setUser: (user) => set((state) => ({ user_id: user })),
             setProfile: (pic) => set((state) => ({ profile_picture: pic })),
             setToken: (tkn) => set((state) => ({ token: tkn })),
             setUsertype: (usrtype) => set((state) => ({ usertype: usrtype })),
+            
+            setCaretakerSchedule: (caretaker_schedule) => set((state) => ({ CaretakerSchedule: caretaker_schedule })),
+
             setEldersList: (arr) => set((state) => ({ elders_list: arr })),
             setElderSchedule: (elder_schdeule) => set((state) => ({ elder_schedule_list: elder_schdeule })),
             removeUserId: () => set({ user_id: null }),
