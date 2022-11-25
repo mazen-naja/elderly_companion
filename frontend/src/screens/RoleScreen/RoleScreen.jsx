025d6ef7c6d1a60e92daf3bd04735d2af85d514f
Mazen Naja <mazennajaa@gmail.com>
2022-11-25 21:37:41 +0200

    Redesigning Role screen

--- frontend/src/screens/RoleScreen/RoleScreen.jsx
+++ frontend/src/screens/RoleScreen/RoleScreen.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from "react";
-import { Text, View ,Button ,TouchableOpacity, TextInput ,Image } from 'react-native';
+import { Text, View ,Button ,TouchableOpacity,ImageBackground ,Image } from 'react-native';
 import styles from "../../../styles";
 
 <StatusBar style="auto" />
@@ -8,15 +8,27 @@ const RoleScreen=({navigation})=>{
 
   
   return (
-    <View style={styles.container}>
-      <Image style={styles.tinyLogo} source={require('../../../assets/logo.jpeg')} />
+    <View style={styles.begincontainer}>
+      
+      <Image style={styles.role_logo} source={require('../../../assets/logo.jpeg')} />
       <Text style={styles.text_title}>Elderly Companion</Text>
-      <Text style={styles.text_subtitle}>Who are you?</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("ElderLogin")} style={styles.button}>
-                  <Text style={styles.role_text}>Elder</Text>
+      <Text style={styles.role_text_subtitle}>Who are you?</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("ElderLogin")} style={styles.role_button}>
+                  <ImageBackground style={styles.background_image} source={require('../../../assets/oldmanicon.jpeg')}>
+                        
+                  </ImageBackground>
+                  <View style={styles.role_text_box}>
+                            <Text style={styles.role_text}>Elder</Text>
+                  </View>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() =>  navigation.navigate("CaretakerLogin")}>
-                  <Text style={styles.role_text}>Caretaker</Text>
+      <TouchableOpacity style={styles.role_button} onPress={() =>  navigation.navigate("CaretakerLogin")}>
+      
+                  <ImageBackground style={styles.background_image} source={require('../../../assets/caretakerlogo.png')}>
+                        
+                  </ImageBackground>
+                        <View style={styles.role_text_box}>
+                                  <Text style={styles.role_text}>Caretaker</Text>
+                        </View>
       </TouchableOpacity>
       
     </View>
