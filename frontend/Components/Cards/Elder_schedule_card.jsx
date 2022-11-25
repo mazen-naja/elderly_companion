40b2527f8433f6ba7899ad28d87ca12242f3884a
Mazen Naja <mazennajaa@gmail.com>
2022-11-25 21:29:22 +0200

    Fixing style

--- frontend/Components/Cards/Elder_schedule_card.jsx
+++ frontend/Components/Cards/Elder_schedule_card.jsx
@@ -7,10 +7,10 @@ import styles from "../../styles";
 const Schedule_card=(props)=>{
     return (
 
-            <View style={styles.flex_column_schedule}>
+            <View style={styles.flex_column}>
                 <View style={styles.flex_row3}>
                 <Text style={styles.card_text}>{props.text}</Text>
-                <Text style={styles.card_text_upper}>Edit</Text>
+                {/* <Text style={styles.card_text_upper}>Edit</Text> */}
                 </View>
                 <View style={styles.flex_row4}>
 
