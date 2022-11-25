e4fd74dfb844f8c5e4e0885e982d854e0ef589fa
Mazen Naja <mazennajaa@gmail.com>
2022-11-25 21:28:44 +0200

    Fixing style

--- frontend/Components/Cards/Elder_caretakers_card.jsx
+++ frontend/Components/Cards/Elder_caretakers_card.jsx
@@ -6,7 +6,7 @@ const Elder_caretakers_card=(props)=>{
     return (
 
 
-<TouchableOpacity flex style={styles.homecard}>
+<TouchableOpacity style={styles.homecard}>
           <Image style={styles.cardicon} source={props.img_src} />
           <Text style={styles.card_text_elder}>{props.caretaker_name}</Text>
 </TouchableOpacity>
